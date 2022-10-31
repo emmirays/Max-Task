696844f1afa29367c78ec0ecaffcccd4188a40c6
emmirays <emmanueloyiboke@gmail.com>
2022-10-31 22:06:20 +0100

    bug fix

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ prompt("What is their name?");
 var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
 
+if (loveScore > 70) {
+  alert(
+    "Your love score is " +
+      loveScore +
+      "%" +
+      "You love each other like Julliet loves Romeo."
+  );
 
-if (loveScore > 70){
-    alert("Your love score is " + loveScore + "%" + "You love each other like Julliet loves Romeo.");
+  if (loveScore > 30 && loveScore <= 70) {
+    alert("Your loveScore is " + loveScore + "%");
+  }
 
-    if (loveScore > 30 && loveScore <= 70){
-      alert("Your loveScore is " + loveScore + "%");
-    }
-
-if (loveScore <= 30){
-      alert("Your loveScore is " + loveScore + "%" + "You go toghether like oil and water");
-    }
-    
+  if (loveScore <= 30) {
+    alert(
+      "Your loveScore is " +
+        loveScore +
+        "%" +
+        "You go toghether like oil and water"
+    );
+  }
 } else {
-    alert("Your love score is " + loveScore + "%");
+  alert("Your love score is " + loveScore + "%");
 }
