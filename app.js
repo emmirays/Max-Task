0e215cc2bdb0f8b6551c37df5a0a318c1dbb8db5
emmirays <emmanueloyiboke@gmail.com>
2022-10-31 21:57:00 +0100

    git push

---
+++ app.js
@@ -0,0 +1,21 @@
+prompt("What is your name?");
+prompt("What is their name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+
+if (loveScore > 70){
+    alert("Your love score is " + loveScore + "%" + "You love each other like Julliet loves Romeo.");
+
+    if (loveScore > 30 && loveScore <= 70){
+      alert("Your loveScore is " + loveScore + "%");
+    }
+
+if (loveScore <= 30){
+      alert("Your loveScore is " + loveScore + "%" + "You go toghether like oil and water");
+    }
+    
+} else {
+    alert("Your love score is " + loveScore + "%");
+}
